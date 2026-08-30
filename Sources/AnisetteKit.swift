adb5c6b147ccc7c385677276dfdb2e7d61192722
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-08-31 01:48:30 +0530

    fix: added compat layer for linux

--- Sources/AnisetteKit.swift
+++ Sources/AnisetteKit.swift
@@ -10,6 +10,28 @@ import Foundation
 import Crypto
 import anisette_core
 
+// for linux, win
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+
+extension URLSession {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                } else {
+                    continuation.resume(throwing: URLError(.unknown))
+                }
+            }
+            task.resume()
+        }
+    }
+}
+#endif
+
 public struct LocalAnisetteHeaders {
     public let machineID: String
     public let oneTimePassword: String
